658bca1c716fefb314fdcfa8166a0c68707e64b9
Zak Tee <zakkturner@yahoo.com>
2022-08-27 20:09:52 -0400

    first commit

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $name = $this->faker->text(25);
+        $description = $this->faker->text(100);
+        $image_url = $this->faker->imageUrl($width = 140, $height = 300);
+        $price = $this->faker->numberBetween($min = 100, $max = 900);
+        return [
+            //
+            'name' => $name,
+            'slug' => Str::slug($name),
+            'description' => $description,
+            'image_name' => $image_url,
+            'price' => $price,
+            'sale_price' => $price - 50,
+
+
+        ];
+    }
+}
